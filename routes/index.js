dd72e81615003eb41de37bc6e33d941cf7cb839e
darshancodepix <darshan@viralmd.com>
2020-07-18 00:28:58 +0530

    get/add/delete blog post

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,110 @@
 const express = require('express');
 const router = express.Router();
 
+const Blog = require('./../models/blog');
+
 router.get('/', (req, res)=>{
     // res.send('hello..');
-    res.render('index');
+    res.render('index',{
+    	title: 'Home',
+    	page_name: 'home'
+    });
 });
+
 router.get('/about', (req, res)=>{
-    res.render('about');
+    res.render('about',{
+    	title: 'About',
+    	page_name: 'about'
+    });
+});
+
+router.get('/about/*', (req, res)=>{
+	res.redirect('/about');
+});
+
+router.get('/post', (req, res)=>{
+	
+    Blog.find().sort({ createdAt: -1})
+        .then((result)=>{
+            res.render('post',{
+                title: 'Post',
+                page_name: 'post',
+                posts: result
+            });
+        })
+        .catch((err)=>{
+            res.render('post',{
+                title: 'Post',
+                page_name: 'post',
+                posts: []
+            });
+        })
+	
+});
+
+router.get('/post/:id', (req, res)=>{
+    var params = req.params;
+    Blog.findById(params.id)
+        .then((result)=>{
+            res.render('post_detail',{
+                title: 'Post',
+                page_name: 'post',
+                post: result
+            });
+        })
+        .catch((err)=>{
+            res.render('post_detail',{
+                title: 'Post',
+                page_name: 'post',
+                post: []
+            });
+        })
+    
+});
+
+router.delete('/delete_post/:id', (req, res)=>{
+    var id = req.params.id;
+    Blog.findByIdAndDelete(id)
+        .then((result)=>{
+            res.json({redirect: '/post'});
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+    
+});
+
+router.post('/add-post', (req, res)=>{
+	
+    const blogs = new Blog({
+         title: req.body.title,
+         snippet: req.body.snippet,
+         description: req.body.description
+    });
+
+     blogs.save()
+         .then((result) =>{
+             res.redirect('/post');
+         })
+         .catch((err) =>{
+             console.log(err);
+         });
+
+});
+
+router.get('/post/create', (req, res)=>{
+    
+    res.render('add_post',{
+        title: 'Add new post',
+        page_name: 'add_post'
+    });
+});
+
+router.use((req, res)=>{
+ 	res.status(404).render('404',{
+    	title: 'Page note found',
+    	page_name: '404'
+    });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 // }
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -13,12 +13,41 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname+ '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
-app.use(express.static('public'));
+app.use(express.static('assest'));
+app.use(express.urlencoded({ extended: true }));
 
 // DB connection
-// mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true, useUnifiedTopology: true});
-// mongoose.connection.on('error', err => {logError(err);});
-// const db = mongoose.connection;
+const mongoURL = "mongodb+srv://root:node123@mynode.zgmzl.mongodb.net/node-project?retryWrites=true&w=majority";
+mongoose.connect(mongoURL,{useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connection.on('error', err => {logError(err);});
+const db = mongoose.connection;
+// root/node123
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://root:node123@mynode.zgmzl.mongodb.net/node-project?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+// const Blog = require('./models/blog');
+// app.get("/add-post", async (req, res) => {
+// 	const blogs = new Blog({
+// 		title: "new blog",
+// 		snippet: "new blog snippet",
+// 		description: "new blog description"
+// 	});
+
+// 	blogs.save()
+// 		.then((result) =>{
+// 			res.send(result);
+// 		})
+// 		.catch((err) =>{
+// 			console.log(err);
+// 		});
+// })
 
 app.use('/', indexRouter);
 
