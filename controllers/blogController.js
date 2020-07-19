7a1a78355ba9d172118a7b4fd4c14aeb16ea228a
darshancodepix <darshan@viralmd.com>
2020-07-19 23:55:53 +0530

    manage view path

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -18,7 +18,7 @@ const blog_post = (req, res) => {
 	
     Blog.find().sort({ createdAt: -1})
         .then((result)=>{
-            res.render('post',{
+            res.render('post/post',{
                 title: 'Post',
                 page_name: 'post',
                 posts: result,
@@ -26,7 +26,7 @@ const blog_post = (req, res) => {
             });
         })
         .catch((err)=>{
-            res.render('post',{
+            res.render('post/post',{
                 title: 'Post',
                 page_name: 'post',
                 posts: []
@@ -39,14 +39,14 @@ const blog_post_detail = (req, res) => {
     var params = req.params;
     Blog.findById(params.id)
         .then((result)=>{
-            res.render('post_detail',{
+            res.render('post/post_detail',{
                 title: 'Post',
                 page_name: 'post',
                 post: result
             });
         })
         .catch((err)=>{
-            res.render('post_detail',{
+            res.render('post/post_detail',{
                 title: 'Post',
                 page_name: 'post',
                 post: []
@@ -56,7 +56,7 @@ const blog_post_detail = (req, res) => {
 }
 const blog_post_create_form = (req, res) => {
 	
-    res.render('add_post',{
+    res.render('post/add_post',{
         title: 'Add new post',
         page_name: 'add_post'
     });
@@ -83,14 +83,14 @@ const blog_post_edit_form = (req, res) => {
     var params = req.params;
     Blog.findById(params.id)
         .then((result)=>{
-            res.render('edit_post',{
+            res.render('post/edit_post',{
                 title: 'Edit Post',
                 page_name: 'edit_post',
                 post: result
             });
         })
         .catch((err)=>{
-            res.render('post_detail',{
+            res.render('post/post_detail',{
                 title: 'Post',
                 page_name: 'post',
                 post: []
